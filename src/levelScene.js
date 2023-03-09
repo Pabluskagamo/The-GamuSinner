65148f4070a070e9ae85478f44ce98eecd46d7f6
CGLMaster <cgomez22@ucm.es>
2023-03-09 17:41:30 +0100

    BlackWolf on map

--- src/levelScene.js
+++ src/levelScene.js
@@ -1,3 +1,4 @@
+import BlackWolf from "./gameobjects/blackWolf"
 import Character from "./gameobjects/character"
 
 export default class LevelScene extends Phaser.Scene {
@@ -9,10 +10,15 @@ export default class LevelScene extends Phaser.Scene {
 	preload() {
 		this.load.image('level_background', '/img/top-down-forest.png')
         this.load.spritesheet('character', '/assets/charac.png', {frameWidth: 64, frameHeight: 64})
+        this.load.spritesheet('blackWolf', '/assets/blackWolf.png', {frameWidth: 64, frameHeight: 64})
 	}
 
 	create() {
 		this.add.image(this.sys.game.canvas.width/2, this.sys.game.canvas.height/2, 'level_background').setScale(2);
         new Character(this, this.sys.game.canvas.width/2, this.sys.game.canvas.height/2, 20);
+		// let rnd = Phaser.Math.RND;
+		// let valueX = rnd.between(1,2);
+		// let valueY = rnd.between(1,2);
+       	new BlackWolf(this, this.sys.game.canvas.width/2, this.sys.game.canvas.height/2, 20);
     }
 }
