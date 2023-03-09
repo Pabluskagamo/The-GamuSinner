58942524ad03bf0be034d38c948c111d97ac9840
CGLMaster <cgomez22@ucm.es>
2023-03-09 15:03:32 +0100

    Button Start

--- src/MainScene.js
+++ src/MainScene.js
@@ -3,19 +3,36 @@ import Phaser from 'phaser'
 export default class HelloWorldScene extends Phaser.Scene {
 	constructor() {
 		super('hello-world')
+
 	}
 
 	preload() {
 		this.load.image('background', '/img/fondo_pixelart3.png')
 		this.load.image('game_title', '/img/titulo.png')
-		// this.load.spritesheet('game_start', '/img/start_sprite.png', 750 , 355, 3);
-		this.load.image('game_start', '/img/start_blanco.png')
+		this.load.spritesheet('game_start', '/img/start_sprite.png', {frameWidth: 750 , frameHeight: 355});
+		//this.load.image('game_start', '/img/start_blanco.png')
 	}
 
 	create() {
+
+		this.anims.create({
+			key: 'hover',
+			frames: this.anims.generateFrameNumbers('game_start', {start: 0, end: 2}),
+			frameRate: 10,
+			repeat: 0
+		})
+
 		this.add.image(0, 0, 'background').setOrigin(0, 0).setScale(0.75);
 		const title = this.add.image(this.sys.game.canvas.width/2, 230, 'game_title').setScale(0.65);
-		const start = this.add.image(this.sys.game.canvas.width/2, 365, 'game_start').setScale(0.35);
+		const start = this.add.sprite(this.sys.game.canvas.width/2, 365, 'game_start').setScale(0.35);
+		start.setInteractive({cursor: 'pointer'});
+		start.on('pointerover', () => {
+			start.play('hover');
+		});
+	
+		start.on('pointerout', () => {
+			start.playReverse('hover');
+		});	
 		
 	}
 }
