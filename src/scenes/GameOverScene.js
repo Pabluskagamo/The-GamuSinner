5a6708971180bfcb58a990cc0c019292b6bc06f0
JavierGil5 <javigi01@ucm.es>
2023-03-25 12:51:09 +0100

    game over

--- src/scenes/GameOverScene.js
+++ src/scenes/GameOverScene.js
@@ -1,6 +1,6 @@
 import Phaser from 'phaser'
 
-export default class MainScene extends Phaser.Scene {
+export default class GameOverScene extends Phaser.Scene {
 	constructor() {
 		super('hello-world')
 
@@ -8,23 +8,23 @@ export default class MainScene extends Phaser.Scene {
 
 	preload() {
 		this.load.image('background', '/img/fondo_pixelart3.png')
-		this.load.image('game_title', '/img/titulo.png')
-		this.load.spritesheet('game_start', '/assets/start_sprite.png', {frameWidth: 750 , frameHeight: 355});
+		this.load.image('game_over', '/img/game_over')
+		//this.load.spritesheet('game_start', '/assets/start_sprite.png', {frameWidth: 750 , frameHeight: 355});
 		//this.load.image('game_start', '/img/start_blanco.png')
 	}
 
 	create() {
 
-		this.anims.create({
-			key: 'hoverStart',
-			frames: this.anims.generateFrameNumbers('game_start', {start: 0, end: 2}),
+		/*this.anims.create({
+			key: 'hover_game_over',
+			frames: this.anims.generateFrameNumbers('game_over', {start: 0, end: 2}),
 			frameRate: 10,
 			repeat: 0
-		})
+		})*/                          
 
 		this.add.image(0, 0, 'background').setOrigin(0, 0).setScale(0.75);
-		const title = this.add.image(this.sys.game.canvas.width/2, 230, 'game_title').setScale(0.65);
-		const start = this.add.sprite(this.sys.game.canvas.width/2, 365, 'game_start').setScale(0.35);
+		const title = this.add.image(this.sys.game.canvas.width/2, 230, 'game_over').setScale(0.65);
+		/*const start = this.add.sprite(this.sys.game.canvas.width/2, 365, 'game_start').setScale(0.35);
 		start.setInteractive({cursor: 'pointer'});
 		start.on('pointerover', () => {
 			start.play('hoverStart');
@@ -42,7 +42,7 @@ export default class MainScene extends Phaser.Scene {
             if (event.keyCode === Phaser.Input.Keyboard.KeyCodes.ENTER) {
                 this.scene.start('instructions');
             }
-        });
+        });*/
 		
 	}
 }
