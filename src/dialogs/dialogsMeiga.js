ab09a36f09bca59291729229188a15a50f689d18
Pabluskagamo <pabluskagamo@gmail.com>
2023-04-26 22:42:46 +0200

    stats

---
+++ src/dialogs/dialogsMeiga.js
@@ -0,0 +1,8 @@
+export default{
+    level1: [
+        "Ohh parece que te has perdido en el bosque...",
+        "¿Cómo? ¿Que estás buscando gamusinos?",
+        "Yo en tu lugar andaría con cuidado",
+        "este bosque no es un lugar para un niño"
+    ]
+}
