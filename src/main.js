6cf79883f869c66c3f8b1b2a5b160cfced1ce6c3
CGLMaster <cgomez22@ucm.es>
2023-04-26 22:12:05 +0200

    Inicio de historyScene

--- src/main.js
+++ src/main.js
@@ -44,7 +44,7 @@ const config = {
 		},
 	},
 
-	scene: [/*MainScene, historyScene, InstructionScene, selecScene,*/ LevelScene4, LevelSceneBoss, LevelScene1, LevelScene2, LevelScene3, Hud, StatsScene, SettingScene, GameOverScene],
+	scene: [/*MainScene,*/ historyScene, /* InstructionScene, selecScene,*/ LevelScene4, LevelSceneBoss, LevelScene1, LevelScene2, LevelScene3, Hud, StatsScene, SettingScene, GameOverScene],
 }
 
 export default new Phaser.Game(config)
