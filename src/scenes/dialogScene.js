bf35737eeb45d2caac114886335509a01d0e4a54
JavierGil5 <javigi01@ucm.es>
2023-04-30 13:30:26 +0200

    dialog scene

--- src/scenes/dialogScene.js
+++ src/scenes/dialogScene.js
@@ -1,7 +1,11 @@
 import Phaser from 'phaser'
+import dialogBox from '../dialogs/dialogBox';
+
 export default class dialogScene extends Phaser.Scene {
     constructor() {
         super('dialog')
+        this.dialogDieguinio;
+        this.dialogAbuelo;
     }
 
     init() {
@@ -16,8 +20,28 @@ export default class dialogScene extends Phaser.Scene {
 
     create() {
         // FONDO
-        this.add.image(0, 0, 'room').setOrigin(0, 0).setScale(1);
-        this.add.image(0, 0, 'historia').setOrigin(0, 0).setScale(1);
+        this.add.image(0, 0, 'room').setOrigin(0, 0).setScale(1).setDepth(0);
+        this.add.image(0, 0, 'historia').setOrigin(0, 0).setScale(1).setDepth(1);
+
+        //DIALOG DIEGUINIO
+        this.dialogDieguinio = new dialogBox(this, 300, 40, 460);
+        this.dialogDieguinio.setDepth(2);
+        this.dialogDieguinio.setFontSize(22);
+        this.dialogDieguinio.setColor('FFFFFF');
+        this.dialogDieguinio.clearText();
+        this.dialogDieguinio.setTextToDisplay('Hola abuelo, estoy aburrido, ¿Qué puedo hacer para pasar el ratiño?');
+        this.dialogDieguinio.printText();
+        //DIALOG ABUELO
+        this.dialogAbuelo = new dialogBox(this, 440, 165, 440);
+        this.dialogAbuelo.setDepth(2);
+        this.dialogAbuelo.setFontSize(22);
+        this.dialogAbuelo.setColor('FFFFFF');
+        this.dialogAbuelo.clearText();
+        this.dialogAbuelo.setTextToDisplay('Pues vete a buscar gamusinos que estoy leyendo y me estás molestando');
+        this.dialogAbuelo.printText();
+
+
+
 
         // SKIP BUTTON
         this.anims.create({
@@ -56,4 +80,6 @@ export default class dialogScene extends Phaser.Scene {
             }
         });
     }
+
+
 }
